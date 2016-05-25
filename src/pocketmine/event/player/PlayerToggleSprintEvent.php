f18c4addfb9193cfba85b57bfa65bd7bee78fc4e
lolcraftboy14 <lolcraftboy13.gamer@gmail.com>
2016-05-25 14:23:36 +0200

    Update PlayerToggleSprintEvent.php

--- src/pocketmine/event/player/PlayerToggleSprintEvent.php
+++ src/pocketmine/event/player/PlayerToggleSprintEvent.php
@@ -39,6 +39,7 @@ class PlayerToggleSprintEvent extends PlayerEvent implements Cancellable{
 		$this->player = $player;
 		$this->isSprinting = (bool) $isSprinting;
 	}
+	
 
 	public function isSprinting(){
 		return $this->isSprinting;
